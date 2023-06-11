ea134d48578da6dadef969d850203183a9c061ab
Brian Martin <brian.beholden@gmail.com>
2023-06-11 07:06:39 -0700

    Initial Commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Brian Martin on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
